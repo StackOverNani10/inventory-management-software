9f0ddb596b4f9a0422a960bc4190dd8c7959c631
Victor Peña Feliz <102447071+VictorpFeliz@users.noreply.github.com>
2024-01-16 17:26:40 -0800

    Update2 Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,3 +1,7 @@
+# Rakefile
+
+require 'rake'
+
 # Tarea para limpiar archivos temporales
 task :clean do
   puts "Limpiando archivos temporales..."
@@ -7,5 +11,14 @@ end
 # Tarea para desplegar la aplicación
 task :deploy => [:clean] do
   puts "Desplegando la aplicación en GitHub Pages..."
+  
+  # Cambia a la rama que deseas desplegar (asegúrate de ajustar el nombre de la rama)
+  sh "git checkout master"
+  
+  # Asegúrate de tener cambios para hacer push (podrías ajustar esto según tu flujo de trabajo)
+  sh "git add -A"
+  sh "git commit -m 'Actualizando para despliegue'"
+  
+  # Realiza el push
   sh "git push origin master"
 end
