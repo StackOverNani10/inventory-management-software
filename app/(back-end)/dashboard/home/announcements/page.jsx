264c27e7c15e04702f75536d1c3cf3f89b58be6f
StackOverNani <1097711@est.intec.edu.do>
2024-01-19 21:45:51 -0400

    Se ha hecho un cambio

--- app/(back-end)/dashboard/home/announcements/page.jsx
+++ app/(back-end)/dashboard/home/announcements/page.jsx
@@ -4,6 +4,9 @@ export default function Announcements() {
   return (
     <div>
         <h2>Announcements</h2>
+        <div>
+          <p>Hola esto es una prueba</p>
+        </div>
     </div>
   )
 }
