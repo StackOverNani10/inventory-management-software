cbd88484e40eb68e7acf80293dbc31e450d47bbc
StackOverNani <1097711@est.intec.edu.do>
2024-01-19 21:05:01 -0400

    updt test files

--- src/__test__/Prueba1.test.js
+++ src/__test__/Prueba1.test.js
@@ -1,5 +1,5 @@
 // mi-modulo.test.js
-const miModulo = require('/Users/danid/OneDrive - INTEC/Escritorio/inventory/inventory-software/src/mi-modulo.js');
+const miModulo = require('src/mi-modulo.js');
 
 test('Mi módulo hace algo', () => {
   // Tu lógica de prueba aquí
