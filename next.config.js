a5f268a3f1d752e618e9af8f6439af2b06ae40af
StackOverNani <1097711@est.intec.edu.do>
2024-03-05 23:50:47 -0400

    Cors permissions added on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // Permitir solicitudes desde el origen específico
+                source: '/api/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Origin', value: 'https://inventory-management-software-roan.vercel.app' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-Requested-With, Content-Type, Authorization' },
+                ],
+            },
+        ]
+    },
     images: {
         remotePatterns: [
             {
